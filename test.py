a9af5d716041800429a6dc176517babb53f98942
SoujanyaHarithsa <132160364+SoujanyaHarithsa@users.noreply.github.com>
2023-11-18 13:13:28 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+#soujanya H S
+#BIT,2024,9019053050,soujanyaharithsa21@gmail.com
+def datastruct(product,price,gst,quantity): 
+    int product=(1,2,3,4)
+    price=(1100,900,200,100)
+    gst=(18,12,28,0)
+    quantity=(1,4,3,2)
+    list1={product[1],price[2],gst[1],quantity[1]}
+    list2={product[2],price[2],gst[2],quantity[2]}
+    list3={product[3],price[3],gst[3],quantity[3]}
+    list4={product[4],price[4],gst[4],quantity[4]}
+    for i in price:
+        if(price>500):
+            price[i]=price-5/100
+max_gst=max(gst)
+total=price[i]
+print(price[i])
+return price[i]
+datastruct(product,price,gst,quantity):
+ int product[]
+ int price[]
+ int gst[]
+ int quantity
+ print(price[i])
